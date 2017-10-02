850d5682563bfbbff6e223573b266c7f97f0bfd1
aaronmlucas <aaron.m.lucas@outlook.com>
2017-10-02 16:17:54 -0400

    Cleaned

--- convert.py
+++ convert.py
@@ -1,5 +1,15 @@
 from OpenSSL import crypto
 
+"""
+
+  Takes a .pfx or .p12 and converts it into .pem format. 
+
+  Files will be output as 
+
+    - CertName.pem (Contains Public Key) and CertName.key.pem (Contains Private Key)
+
+"""
+
 CertName = raw_input("What's the Certificate URL?: ")
 OldCertFile = raw_input("Original Certificate file?: ")
 CertPass = raw_input("Cert Password?: ")
